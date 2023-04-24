8e92d6786796e482425d11a458960bf990e5d9c4
susanee <comtrial97@gmail.com>
2023-04-24 17:28:44 +0900

    feat: N 과M 2

--- PS/BaekJoon/N과M2.java
+++ PS/BaekJoon/N과M2.java
@@ -6,8 +6,15 @@ public class N과M2 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt(), m = sc.nextInt();
+        int n = sc.nextInt(), m = sc.nextInt(); //4 2
+        //1 2
+        //1 3
+        //1 4
+        //2 3
+        //2 4
+        //3 4
 
-
+        System.out.println(n);
+        System.out.println(m);
     }
 }
--- PS/Programmers/소수_찾기.java
+++ PS/Programmers/소수_찾기.java
@@ -1,10 +1,13 @@
 package Programmers;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 
 // https://school.programmers.co.kr/learn/courses/30/lessons/42839
 class Solution {
     static int answer;
+    static HashSet<String> answerSet;
     static String[] nodeArr;
     static boolean[] visit;
     // 1. 방문한 노드에 대한 결과 값에 대한 추적이 제대로 이루어지고 있지 않다.
@@ -16,23 +19,30 @@ class Solution {
         int answer = 0;
         nodeArr = numbers.split("");
         visit = new boolean[nodeArr.length];
-        // 2. 스플릿된 모든 문자열의 조합 만들기
-        //      if 0 으로 시작하면 안대j
-//        for (int i=0; i<nodeArr.length; i++)
+        answerSet = new HashSet<String>();
         dfs(0, 0, "");
+
+        for (String comb : answerSet) {
+            if (isPrime(Integer.parseInt(comb))) {
+                System.out.println(comb);
+                answer++;
+            }
+        }
     }
 
     private void dfs(int depth, int index, String comb) {
 
         // 기본 실행
         if (comb != ""){
-            if (isPrime(Integer.parseInt(comb))) this.answer += 1;
+            if (isPrime(Integer.parseInt(comb))) {
+                answerSet.add(comb);
+            }
         }
-        System.out.println(comb);
+//        System.out.println(comb);
 
         // 종료 좋건
 //        if (!Arrays.asList(visit).contains(false)) return;
-        if (depth == nodeArr.length) return;
+        if (depth == nodeArr.length || comb.equals("0")) return;
 
         // 기본 실행 재귀
         for (int i = 0; i < nodeArr.length; i++) {
@@ -40,14 +50,17 @@ class Solution {
                 visit[i] = true;
                 comb += nodeArr[i];
                 dfs(depth+1, i, comb);
+                comb = comb.substring(0, comb.length() - 1);
                 visit[i] = false;
                 }
         }
     }
 
     public boolean isPrime(int n) {
+        if (n==0 || n ==1) return false;
+
         for (int i = 2; i <= (int) Math.sqrt(n); i++)
-            if (n % i == 0) return false;
+            if (n==1 | n % i == 0) return false;
 
         return true;
     }
@@ -55,7 +68,7 @@ class Solution {
 
     public static void main(String[] args) {
         Solution sol = new Solution();
-        sol.solution("17"); //3
+        sol.solution("011"); //3
         System.out.println("ssss" + answer);
     }
 }
